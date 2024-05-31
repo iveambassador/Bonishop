283a546218fe6000524a66e9202a7720241a8794
iveambassador <IvelizAlexandra.AyalaLopez@studentambassadors.com>
2024-05-30 23:40:52 -0400

    new change to stopdev

--- stop-dev.js
+++ stop-dev.js
@@ -3,6 +3,3 @@ const process = require('process');
 
 // Obtener el proceso principal (puedes ajustar el valor según tu configuración)
 const mainProcessPid = process.pid;
-
-// Enviar la señal de interrupción (SIGINT) al proceso principal
-process.kill(mainProcessPid, 'SIGINT');
